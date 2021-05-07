580238e75df275216fe55b55ef75d193e19af99d
pdkary <pdkary@gmail.com>
2021-05-07 06:01:41 -0400

    added notes for menglin to read in the morning

--- ValorantMarketPlace.sol
+++ ValorantMarketPlace.sol
@@ -5,164 +5,231 @@ import "./PlayerToken.sol";
 
 contract ValorantMarketPlace {
     using SafeMath for *;
-    
+
     address owner1;
     address owner2;
 
     uint256 pot;
-    mapping (string => uint256) tokenPrices;
-    mapping (string => bool) tokenListings;
-    mapping (string => PlayerToken) tokens;
-    mapping (address => string[]) heldTokens;
-    
+    mapping(string => uint256) tokenPrices;
+    mapping(string => bool) tokenListings;
+    mapping(string => PlayerToken) tokens;
+    mapping(address => string[]) heldTokens;
+
     uint256 initial_supply = 1e5;
     uint8 decimals = 4;
-    
+
     uint256 fee_percent = 5; //5% vig
-    
-    event Buy(address buyer, string playerID, uint256 price, uint256 num_tokens);
-    event Sell(address seller, string playerID, uint256 price, uint256 num_tokens);
-    event Mint(string playerID,string tokenName,string tokenSymbol,uint256 tokenPrice);
-    event Creation(address owner1,address owner2, uint256 pot);
-    
+
+    event Buy(
+        address buyer,
+        string playerID,
+        uint256 price,
+        uint256 num_tokens
+    );
+    event Sell(
+        address seller,
+        string playerID,
+        uint256 price,
+        uint256 num_tokens
+    );
+    event Mint(
+        string playerID,
+        string tokenName,
+        string tokenSymbol,
+        uint256 tokenPrice
+    );
+    event Creation(address owner1, address owner2, uint256 pot);
+
     modifier onlyOwner {
         require(msg.sender == owner1 || msg.sender == owner2);
         _;
     }
-    
-    constructor(address other_owner) payable {
+
+    constructor(address second_owner) payable {
         owner1 = msg.sender;
-        owner2 = other_owner;
+        owner2 = second_owner;
         pot = msg.value;
-        emit Creation(owner1,owner2,pot);
+        emit Creation(owner1, owner2, pot);
     }
-    
+
     function deposit() public payable onlyOwner {
-        pot = SafeMath.add(pot,msg.value);
+        pot = SafeMath.add(pot, msg.value);
     }
-    
+
     function withdraw() public payable onlyOwner {
-        pot = SafeMath.sub(pot,msg.value);
+        pot = SafeMath.sub(pot, msg.value);
     }
-    
-    function mintToken(string calldata playerID,string calldata tokenName,string calldata tokenSymbol, uint256 tokenPrice) public onlyOwner returns (bool) {
+
+    function mintToken(
+        string calldata playerID,
+        string calldata tokenName,
+        string calldata tokenSymbol,
+        uint256 tokenPrice
+    ) public onlyOwner returns (bool) {
         require(!tokenListings[playerID]);
-        tokens[playerID] = new PlayerToken(tokenName,tokenSymbol,initial_supply,decimals);
+        tokens[playerID] = new PlayerToken(
+            tokenName,
+            tokenSymbol,
+            initial_supply,
+            decimals
+        );
         tokenListings[playerID] = true;
         tokenPrices[playerID] = tokenPrice;
-        emit Mint(playerID,tokenName,tokenSymbol,tokenPrice);
+        emit Mint(playerID, tokenName, tokenSymbol, tokenPrice);
         return true;
     }
+
     function getPrice(string calldata playerID) public view returns (uint256) {
         require(tokenListings[playerID]);
         return tokenPrices[playerID];
     }
-    
-    function getTotalSupply(string calldata playerID) public view returns (uint256) {
+
+    function getTotalSupply(string calldata playerID)
+        public
+        view
+        returns (uint256)
+    {
         require(tokenListings[playerID]);
         PlayerToken token = tokens[playerID];
         return token.totalSupply();
     }
-    
-    function getTokenBalance(string calldata playerID, address user) public view returns (uint256) {
+
+    function getTokenBalance(string calldata playerID, address user)
+        public
+        view
+        returns (uint256)
+    {
         require(tokenListings[playerID]);
         PlayerToken token = tokens[playerID];
         return token.balanceOf(user);
     }
-    
-    function updatePrice(string calldata playerID,uint256 price) public onlyOwner {
+
+    function updatePrice(string calldata playerID, uint256 price)
+        public
+        onlyOwner
+        returns (bool)
+    {
         require(tokenListings[playerID]);
         tokenPrices[playerID] = price;
+        return true;
     }
-    
-    function getTokenValue(string calldata playerID,address user) public view returns (uint256 value){
+
+    function getTokenValue(string calldata playerID, address user)
+        public
+        view
+        returns (uint256 value)
+    {
         require(tokenListings[playerID]);
         PlayerToken token = tokens[playerID];
-        value = SafeMath.mul(token.balanceOf(user),tokenPrices[playerID]);
+        value = SafeMath.mul(token.balanceOf(user), tokenPrices[playerID]);
         return value;
     }
-    
+
     function getPot() public view returns (uint256) {
         return pot;
     }
-    
+
     function getHeldTokens(address user) public view returns (string[] memory) {
         return heldTokens[user];
     }
-    
-    function buyToken(string calldata playerID) external payable returns(bool) {
+
+    function buyToken(string calldata playerID)
+        external
+        payable
+        returns (bool)
+    {
         require(tokenListings[playerID]);
         PlayerToken token = tokens[playerID];
-        
+
         uint256 tokenPrice = tokenPrices[playerID];
-        uint256 cost = SafeMath.mul((100+fee_percent),tokenPrice) / 100;
-        
-        uint256 tokens_received = msg.value/cost;
-        
+        uint256 multiplier = (100 + fee_percent) * (10**(decimals - 2));
+        uint256 cost = SafeMath.mul(multiplier, tokenPrice);
+
+        uint256 tokens_received = msg.value / cost;
+
         //assert that mesage holds enough value for this transaction
         require(msg.value >= cost);
         bool success = true;
         //approve tokens for transfer from owner
-        success = success && token.approve(msg.sender,tokens_received);
+        success = success && token.approve(msg.sender, tokens_received);
         //transfer tokens
-        success = success && token.transferFrom(address(this),msg.sender,tokens_received);
-        
-        emit Buy(msg.sender,playerID,tokenPrice,tokens_received);
+        success =
+            success &&
+            token.transferFrom(address(this), msg.sender, tokens_received);
+
+        emit Buy(msg.sender, playerID, tokenPrice, tokens_received);
         heldTokens[msg.sender].push(playerID);
-        
+
         //update pot
-        pot = SafeMath.add(pot,msg.value);
+        pot = SafeMath.add(pot, msg.value);
         return success;
     }
-    
-    function approveSale(string calldata playerID, uint256 count) external payable returns (bool) {
-       PlayerToken token = tokens[playerID];
-       return token.approve_reverse(msg.sender,count);
-    }
-    
-    function sellToken(string calldata playerID, uint256 count) external payable {
+
+    function approveSale(string calldata playerID, uint256 count)
+        external
+        payable
+        returns (bool)
+    {
         PlayerToken token = tokens[playerID];
-        
+        return token.approve_reverse(msg.sender, count);
+    }
+
+    function sellToken(string calldata playerID, uint256 count)
+        external
+        payable
+        returns (bool)
+    {
+        PlayerToken token = tokens[playerID];
+
         uint256 tokenPrice = tokenPrices[playerID];
-        uint256 cost = SafeMath.mul(100,tokenPrice) / (100+fee_percent);
-        uint256 allowed = token.allowance(msg.sender,address(this));
+        uint256 multiplier = (10**(decimals + 2)) / (100 + fee_percent);
+        uint256 cost = SafeMath.mul(multiplier, tokenPrice);
+        uint256 allowed = token.allowance(msg.sender, address(this));
+
         require(allowed >= count);
         //make sure user has tokens to sell
         uint256 tokenBalance = token.balanceOf(msg.sender);
         require(tokenBalance >= count);
         //make sure this wont bankrupt us
-        uint256 payout = SafeMath.mul(cost,count);
+        uint256 payout = SafeMath.mul(cost, count);
         require(pot >= payout);
-        
+
         //return tokens to the house, get money
-        token.transferFrom(msg.sender,address(this),count);
+        token.transferFrom(msg.sender, address(this), count);
         payable(msg.sender).transfer(payout);
-        if(tokenBalance - count == 0){
-            remove_from_holdings(msg.sender,playerID);
+        if (tokenBalance - count == 0) {
+            remove_from_holdings(msg.sender, playerID);
         }
-        
-        emit Sell(msg.sender,playerID,tokenPrice,count);
-        
+
+        emit Sell(msg.sender, playerID, tokenPrice, count);
+
         //pot -= msg.value;
-        pot = SafeMath.sub(pot,msg.value);
+        pot = SafeMath.sub(pot, msg.value);
+        return true;
     }
-    
+
     //--------------------------------------------UTILITY FUNCTIONS -------------------------------------------//
-    function compare_strings(string memory a, string memory b) internal pure returns (bool) {
-        return (keccak256(abi.encodePacked(a)) == keccak256(abi.encodePacked(b)));
+    function compare_strings(string memory a, string memory b)
+        internal
+        pure
+        returns (bool)
+    {
+        return (keccak256(abi.encodePacked(a)) ==
+            keccak256(abi.encodePacked(b)));
     }
-    
-    function remove_from_holdings(address holder, string memory playerID) internal {
-        
+
+    function remove_from_holdings(address holder, string memory playerID)
+        internal
+    {
         string[] memory holds = heldTokens[holder];
         uint256 i = 0;
         bool found = false;
         uint256 len = holds.length;
-        for(i=0;i<len;i++){
+        for (i = 0; i < len; i++) {
             string memory looking = holds[i];
-            if(compare_strings(playerID,looking))found=true;
-            if(found){
-                if(i!=len-1)holds[i]=holds[i+1];
+            if (compare_strings(playerID, looking)) found = true;
+            if (found) {
+                if (i != len - 1) holds[i] = holds[i + 1];
                 else {
                     delete holds[i];
                 }
